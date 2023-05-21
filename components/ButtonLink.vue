0645ccf62a4ce006b8c0b15ab949b8b511c3edaf
Bishesh <bishesh.sunam@gmail.com>
2023-05-21 12:02:15 +1200

    feat: add sidebar links

---
+++ components/ButtonLink.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+<template>
+  <div
+    class="w-full h-8 p-4 flex gap-2 items-center hover:bg-white/30 hover:cursor-pointer"
+  >
+    <NuxtLink to="https://nuxtjs.org"> 
+        icon title icon
+    </NuxtLink>
+  </div>
+</template>
+<style lang="scss" scoped></style>
--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,8 +1,34 @@
 <script setup></script>
 <template>
   <div
-    class="w-60 absolute top-[50px] left-0 px-2.5 h-[96vh] border border-slate-400 backdrop-blur-md bg-white/30"
+    class="w-60 absolute top-[50px] left-0 h-[96vh] border border-slate-400 backdrop-blur-md bg-white/30 flex flex-col justify-between"
   >
-    side bar..
+    <!-- top -->
+    <div class="border-b border-slate-400 p-4">
+      icon
+      <div>
+        <h1>bishesh's workspaces</h1>
+        <p>Free</p>
+      </div>
+      icon
+    </div>
+    <!-- middle -->
+    <div class="overflow-auto flex-grow">
+      <ButtonLink />
+      <ButtonLink />
+      <ButtonLink />
+      <h1 class="p-4">Workspace views</h1>
+      <ButtonLink />
+      <ButtonLink />
+      <button class="p-4">Your boards</button>
+      <ButtonLink />
+      <ButtonLink />
+      <ButtonLink />
+    </div>
+
+    <!-- bottom -->
+    <div class="border-t border-slate-400">
+      <button class="p-4">Try Premium free</button>
+    </div>
   </div>
 </template>
