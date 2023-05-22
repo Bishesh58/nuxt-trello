9875cbf6ed1f18eac2de55bf49cef384bf0b246b
Bishesh <bishesh.sunam@gmail.com>
2023-05-22 21:21:42 +1200

    chore: add vuedraggable library

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,70 @@
+<script setup>
+import draggable from "vuedraggable";
+
+const log = (ev) => {
+  console.log(ev);
+};
+
+const list1 = ref([
+  { name: "John", id: 1 },
+  { name: "Joao", id: 2 },
+  { name: "Jean", id: 3 },
+  { name: "Gerard", id: 4 },
+  { name: "Juan", id: 5 },
+  { name: "Edgard", id: 6 },
+  { name: "Johnson", id: 7 },
+]);
+
+const list2 = ref([]);
+</script>
+
 <template>
-  <div>test</div>
+  <div class="m-20 p-32 border rounded-md flex justify-center items-center h-full">
+    <!-- drag containter 1 -->
+    <draggable
+      class="list-group"
+      v-model="list1"
+      group="ticket"
+      @change="log"
+      itemKey="name"
+      handle=".handle"
+    >
+      <template #header>
+        <h1 class="px-4">Open Ticket</h1>
+      </template>
+      <template #item="{ element }">
+        <div class="list-group-item">
+          {{ element.name }}
+        </div>
+      </template>
+    </draggable>
+    <!-- drag container 2 -->
+    <draggable
+      class="list-group"
+      v-model="list2"
+      group="ticket"
+      @change="log"
+      itemKey="name"
+      handle=".handle"
+    >
+      <template #header>
+        <h1 class="px-4">Assign ticket</h1>
+      </template>
+      <template #item="{ element }">
+        <div class="list-group-item">{{ element.name }}</div>
+      </template>
+    </draggable>
+  </div>
 </template>
 
-<script setup></script>
-
-<style scoped></style>
+<style scoped>
+.list-group {
+  width: 500px;
+}
+.list-group-item {
+  padding: 20px;
+  margin: 5px;
+  background: whitesmoke;
+  border-radius: 10px;
+}
+</style>
