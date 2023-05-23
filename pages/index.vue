47adf1ac63da804e02cda1a7208c6837abf80c1e
Bishesh <bishesh.sunam@gmail.com>
2023-05-23 18:11:38 +1200

    refactor: remove vue-draggable-next add vuedraggable

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,30 @@
-<script setup>
-import draggable from "vuedraggable";
-
-const log = (ev) => {
-  console.log(ev);
-};
-
-const list1 = ref([
-  { name: "John", id: 1 },
-  { name: "Joao", id: 2 },
-  { name: "Jean", id: 3 },
-  { name: "Gerard", id: 4 },
-  { name: "Juan", id: 5 },
-  { name: "Edgard", id: 6 },
-  { name: "Johnson", id: 7 },
-]);
-
-const list2 = ref([]);
-</script>
-
 <template>
-  <div class="m-20 p-32 border rounded-md flex justify-center items-center h-full">
-    <!-- drag containter 1 -->
-    <draggable
-      class="list-group"
-      v-model="list1"
-      group="ticket"
-      @change="log"
-      itemKey="name"
-      handle=".handle"
-    >
-      <template #header>
-        <h1 class="px-4">Open Ticket</h1>
-      </template>
-      <template #item="{ element }">
-        <div class="list-group-item">
-          {{ element.name }}
-        </div>
-      </template>
-    </draggable>
-    <!-- drag container 2 -->
-    <draggable
-      class="list-group"
-      v-model="list2"
-      group="ticket"
-      @change="log"
-      itemKey="name"
-      handle=".handle"
-    >
-      <template #header>
-        <h1 class="px-4">Assign ticket</h1>
-      </template>
-      <template #item="{ element }">
-        <div class="list-group-item">{{ element.name }}</div>
-      </template>
-    </draggable>
+  <div class="bg-slate-400 h-full flex justify-center items-center">
+    <ClientOnly>
+      <draggable
+        v-model="myArray"
+        group="people"
+        @start="drag = true"
+        @end="drag = false"
+        item-key="id"
+      >
+        <template #item="{ element }">
+          <div>{{ element.name }}</div>
+        </template>
+      </draggable>
+    </ClientOnly>
   </div>
 </template>
 
-<style scoped>
-.list-group {
-  width: 500px;
-}
-.list-group-item {
-  padding: 20px;
-  margin: 5px;
-  background: whitesmoke;
-  border-radius: 10px;
-}
-</style>
+<script setup>
+const myArray = ref([
+  { name: "test 1" },
+  { name: "test 2" },
+  { name: "test 3" },
+  { name: "test 4" },
+  { name: "test 5" },
+  { name: "test 6" },
+]);
+</script>
+
+<style scoped></style>
--- plugins/draggable.js
+++ plugins/draggable.js
@@ -2,4 +2,5 @@ import draggable from "vuedraggable";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component("draggable", draggable);
+  // draggable: Error on nuxt version 3.5.0
 });
