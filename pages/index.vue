ef5503a9c6e43d40fe63f5bad023179b0ec49aa1
Bishesh <bishesh.sunam@gmail.com>
2023-05-27 23:04:20 +1200

    refactor: remove transition from modal

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,11 @@
   <div class="">
     <div class="flex gap-2">
       <list-container v-for="item in boards" :key="item.id" :list="myArray" />
-      <div class="min-w-[250px] rounded-lg bg-white">
-        <!-- <button
-          @click="addBoard"
+      <div class="min-w-[250px] rounded-lg bg-white relative">
+        <button
+          @click="showModal = true"
+          v-show="!showModal"
+          ref="showModalButton"
           class="flex-1 py-1 px-2.5 bg-gray-400/40 hover:bg-gray-400/60 rounded-md w-full text-left"
         >
           <IconsPlus
@@ -14,9 +16,15 @@
             class="inline mr-2"
           />
           Add a board
-        </button> -->
-        <div class="m-2">
+        </button>
+
+        <div
+          id="modal"
+          v-if="showModal"
+          class="absolute top-0 bg-white p-2 rounded-md"
+        >
           <input
+            ref="modalInput"
             type="text"
             placeholder="Enter board title"
             class="w-full p-2 mb-1 outline-none text-sm placeholder:text-gray-800 rounded-md focus:ring-2 focus:ring-blue-700 focus:ring-inset"
@@ -27,6 +35,7 @@
             Add board
           </button>
           <IconsClose
+            @click="showModal = false"
             width="30"
             height="30"
             color="gray"
@@ -39,6 +48,34 @@
 </template>
 
 <script setup>
+// modal
+const showModal = ref(false);
+const showModalButton = ref(null);
+const modalInput = ref(null);
+
+const closeModal = () => {
+  showModal.value = false;
+};
+
+onMounted(() => {
+  document.addEventListener("click", handleOutsideClick);
+});
+
+onUnmounted(() => {
+  document.removeEventListener("click", handleOutsideClick);
+});
+
+const handleOutsideClick = (ev) => {
+  const modalContainer = document.getElementById("modal");
+  if (
+    modalContainer &&
+    !modalContainer.contains(ev.target) &&
+    ev.target !== showModalButton.value
+  ) {
+    closeModal();
+  }
+};
+
 const myArray = ref([
   { id: 1, name: "test 1" },
   { id: 2, name: "test 2" },
@@ -47,7 +84,7 @@ const myArray = ref([
   { id: 5, name: "test 5" },
   { id: 6, name: "test 6" },
 ]);
-const visible = ref(false);
+
 const numBoards = ref(0); // Number of initial boards
 
 const addBoard = () => {
@@ -60,6 +97,14 @@ const boards = computed(() => {
     currentBoards.push({ id: i + 1, name: `Board ${i + 1}` });
   }
   return currentBoards;
+});
+
+watch(showModal, (newValue) => {
+  if (newValue) {
+    nextTick(() => {
+      modalInput.value.focus();
+    });
+  }
 });
 </script>
 
