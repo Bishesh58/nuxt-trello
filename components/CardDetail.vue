9cee5f62a25167942f2055a4def8382c1af08722
Bishesh <bishesh.sunam@gmail.com>
2023-06-06 20:14:18 +1200

    chore: add style to card detail

--- components/CardDetail.vue
+++ components/CardDetail.vue
@@ -6,7 +6,7 @@
     :draggable="false"
     position="center"
     appendTo="#feed"
-    class="min-w-[480px] bg-[#f1f2f4] md:w-full max-w-[800px] h-full"
+    class="min-w-[480px] bg-[#f1f2f4] md:w-full max-w-[800px] h-full mx-2"
   >
     <template #header>
       <h1 class="text-2xl text-gray-700 font-bold">
@@ -21,7 +21,7 @@
       </div>
     </template>
     <!-- #default -->
-    <div class="flex">
+    <div class="h-full flex flex-col md:flex-row bg-[#f1f2f4] text-sm">
       <!-- right -->
       <div class="flex-grow">
         <!-- top -->
@@ -50,12 +50,110 @@
         </div>
       </div>
       <!-- left -->
-      <div class="w-[200px] bg-red-400">
+      <div class="grid grid-cols-2 gap-2 md:w-[200px] md:grid-cols-1 h-fit">
         <div
-          class="h-full p-1 px-1.5 rounded-md hidden md:flex justify-center items-center hover:bg-white/30 hover:cursor-pointer"
+          class="px-2 flex justify-between items-center gap-2rounded-sm col-span-2 md:col-span-1"
         >
-          <IconsCheckList width="22" height="22" color="white" />
-          <IconsClock width="22" height="22" color="white" />
+          <span class="font-semibold">Suggested</span>
+
+          <div
+            class="h-6 w-6 p-1 rounded-md flex justify-center items-center bg-gray-200 hover:bg-gray-300 cursor-pointer"
+          >
+            <IconsSettings width="18" height="18" color="#374151" />
+          </div>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Join</span>
+        </div>
+        <div class="p-2 col-span-2 md:col-span-1">
+          <span>Add to card</span>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Members</span>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Labels</span>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Checklist</span>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Dates</span>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Attachment</span>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Cover</span>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Custom Fields</span>
+        </div>
+        <div class="p-2 col-span-2 md:col-span-1">
+          <span>Power-Ups</span>
+        </div>
+        <div class="hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm">
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Add Power-Ups</span>
+        </div>
+        <div class="p-2 col-span-2 md:col-span-1">
+          <span>Actions</span>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Move</span>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Copy</span>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Make template</span>
+        </div>
+        <hr class="p-2 col-span-2 md:col-span-1" />
+
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Archive</span>
+        </div>
+        <div
+          class="bg-gray-200 hover:bg-gray-300 p-2 flex gap-2 items-center rounded-sm"
+        >
+          <IconsUser width="22" height="22" color="#374151" />
+          <span>Share</span>
         </div>
       </div>
     </div>
