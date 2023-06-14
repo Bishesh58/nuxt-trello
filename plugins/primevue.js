bd16cbdc60ac3d2040d96a58276f0966a3cd22e4
Bishesh <bishesh.sunam@gmail.com>
2023-06-14 21:04:49 +1200

    chore: remove prive editor

--- plugins/primevue.js
+++ plugins/primevue.js
@@ -2,11 +2,9 @@ import { defineNuxtPlugin } from "#app";
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
 import Dialog from "primevue/dialog";
-import Editor from 'primevue/editor';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, { ripple: true });
   nuxtApp.vueApp.component("Button", Button);
   nuxtApp.vueApp.component("Dialog", Dialog);
-  nuxtApp.vueApp.component("Editor", Editor);
 });
